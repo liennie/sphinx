26cab1aa3c981871cc41a03c1f3421aa8606979a
Liennie <trbola.martin@seznam.cz>
2025-06-18 21:28:33 +0200

    Clock format

--- data/puzzles/index/script.js
+++ data/puzzles/index/script.js
@@ -10,7 +10,7 @@
 
 	const updateTime = () => {
 		const now = new Date();
-		const end = new Date('2025-06-19T01:25:00');
+		const end = new Date('2025-06-25T00:00:00');
 		let diff = end - now;
 
 		if (diff <= 0) {
@@ -39,7 +39,7 @@
 		ttl.setAttribute("data-digit", Math.floor(seconds / 10));
 		tol.setAttribute("data-digit", seconds % 10);
 
-		document.title = `${days.toString().padStart(2, "0")}:${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+		document.title = `${days.toString().padStart(2, "0")} : ${hours.toString().padStart(2, "0")} : ${minutes.toString().padStart(2, "0")} : ${seconds.toString().padStart(2, "0")}`;
 	};
 	setInterval(updateTime, 1000);
 	updateTime();
